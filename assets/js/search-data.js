7e5369c5f3536a8c279a67fe99f8c829ba953c85
leibigzhen <zhenlei@seu.edu.cn>
2025-12-03 12:40:10 +0000

    Deploying to gh-pages from @ leibigzhen/leibigzhen.github.io@fa9e82dd5d9e408c29df5308448128d3d591a199 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-energy-system-transformation-an-exciting-nordic-trip-️-to-sweden-and-the-netherlands",
+          title: 'Energy System Transformation: An exciting Nordic trip 🏙️ to Sweden and the Netherlands....',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
